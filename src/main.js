6a7195449164a08a2090f37773437cdcc59fe36a
raghavendrah25 <152840271+raghavendrah25@users.noreply.github.com>
2025-02-21 15:08:29 +0530

    Updated

--- src/main.js
+++ src/main.js
@@ -4,5 +4,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import firebase from 'firebase/compat/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzLjnRsCJ2CeBjOxBBPHfjpk6qFpiQkKY",
+  authDomain: "e-commerce-f2e3f.firebaseapp.com",
+  projectId: "e-commerce-f2e3f",
+  storageBucket: "e-commerce-f2e3f.firebasestorage.app",
+  messagingSenderId: "126514619345",
+  appId: "1:126514619345:web:d3489159f82397050db6f0"
+};
+firebase.initializeApp(firebaseConfig);
+
 
 createApp(App).mount('#app')
--- src/webservice/index.js
+++ src/webservice/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 function getProducts() {
-  return axios.get("http://0.0.0.0:3000/products");
+  return axios.get("http://0.0.0.0:8080/getProducts");
 }
 
 function getCategory() {
-  return axios.get("http://0.0.0.0:3000/categories");
+  return axios.get("http://0.0.0.0:8080/categories");
 }
 
 export default {
